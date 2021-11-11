dea383f0aed03bec0edf273197b5e4f89f834ac7
linata81 <linata81@gmail.com>
2021-11-11 17:47:00 +0300

    made services

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { MainPage, CartPage } from '../pages';
 import AppHeader from '../app-header';
-import WithRestoService from '../hoc';
 
 import Background from './food-bg.jpg';
 
-const App = ({ RestoService }) => {
-    console.log(RestoService.getMenuItems());
+const App = () => {
+
     return (
         <div style={{ background: `url(${Background}) center center/cover no-repeat` }} className="app">
             <AppHeader total={50} />
@@ -16,4 +15,4 @@ const App = ({ RestoService }) => {
     )
 }
 
-export default WithRestoService()(App);
+export default App;
--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -1,19 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import MenuListItem from '../menu-list-item';
+import { connect } from 'react-redux';
+import WithRestoService from '../hoc';
 
 import './menu-list.scss';
 
 class MenuList extends Component {
 
+    componentDidMount() {
+        const { RestoService } = this.props;
+        RestoService.getMenuItems()
+            .then(res => console.log(res));
+    }
+
     render() {
+        const { menuItems } = this.props;
 
         return (
             <ul className="menu__list">
-                <MenuListItem/>
+                {
+                    menuItems.map(menuItem => {
+                        return <MenuListItem key={menuItem.id} menuItem={menuItem} />
+                    })
+                }
             </ul>
         )
     }
 };
 
+const mapStateToProps = (state) => {
+    return {
+        menuItems: state.menu
+    }
+}
 
-export default MenuList;
+export default WithRestoService()(connect(mapStateToProps)(MenuList));
