4777ce8d55591b87e758659d8e383a9e1f5ce0ce
linata81 <linata81@gmail.com>
2021-11-23 14:11:12 +0300

    router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { MainPage, CartPage } from '../pages';
 import AppHeader from '../app-header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
 const App = () => {
 
     return (
-        <div style={{ background: `url(${Background}) center center/cover no-repeat` }} className="app">
-            <AppHeader total={50} />
-            <MainPage />
-            <CartPage />
-        </div>
+        <Router>
+            <div className="app" style={{ background: `url(${Background}) center center/cover no-repeat` }} >
+                <AppHeader total={50} />
+                <Route path='/' exact component={MainPage} />
+                <Route path='/cart' exact component={CartPage} />
+            </div>
+        </Router>
     )
 }
 
--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import MenuListItem from '../menu-list-item';
 import { connect } from 'react-redux';
 import WithRestoService from '../hoc';
-import { menuLoaded, menuRequested } from '../../actions';
+import { menuLoaded, menuRequested, addedToCart } from '../../actions';
 import Spinner from '../spinner';
 
 import './menu-list.scss';
@@ -18,7 +18,7 @@ class MenuList extends Component {
     }
 
     render() {
-        const { menuItems, loading } = this.props;
+        const { menuItems, loading, addedToCart } = this.props;
 
         if (loading) {
             return <Spinner />
@@ -28,7 +28,10 @@ class MenuList extends Component {
             <ul className="menu__list">
                 {
                     menuItems.map(menuItem => {
-                        return <MenuListItem key={menuItem.id} menuItem={menuItem} />
+                        return <MenuListItem
+                            key={menuItem.id}
+                            menuItem={menuItem}
+                            onAddToCart={() => addedToCart(menuItem.id)} />
                     })
                 }
             </ul>
@@ -45,7 +48,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = {
     menuLoaded,
-    menuRequested
+    menuRequested,
+    addedToCart
 }
 
 export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(MenuList));
